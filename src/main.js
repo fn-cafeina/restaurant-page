4943fd9dea2bdc638210306a47ece40f698b8526
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-06-03 11:54:23 -0600

    feat: add logic to change beetween sections

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import "./base.css";
 
-import menu from "./menu";
+import { cleanCtn } from "./utils";
 
-document.getElementById("content").appendChild(menu);
+import home from "./home";
+import menu from "./menu";
+import about from "./about";
+
+const content = document.getElementById("content");
+content.appendChild(home);
+
+const homeBtn = document.querySelector(".home-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
+homeBtn.addEventListener("click", changeContent);
+menuBtn.addEventListener("click", changeContent);
+aboutBtn.addEventListener("click", changeContent);
+
+function changeContent(event) {
+  cleanCtn(content);
+
+  if (event.target.textContent === "Home") {
+    content.appendChild(home);
+  }
+
+  if (event.target.textContent === "Menu") {
+    content.appendChild(menu);
+  }
+
+  if (event.target.textContent === "About") {
+    content.appendChild(about);
+  }
+}
