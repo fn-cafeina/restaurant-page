aeb6ffb1e2dda527a725304721debf2bafe522aa
fn-cafeina <201848984+fn-cafeina@users.noreply.github.com>
2025-06-02 21:22:14 -0600

    chore: remove function from utils

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,3 @@
-export function createHeader(text, clase = "") {
-  const div = document.createElement("div");
-  const p = document.createElement("p");
-
-  if (clase) div.classList.add(clase);
-
-  p.textContent = text;
-
-  div.appendChild(p);
-
-  return div;
-}
-
 export function cleanCtn(ctn) {
   while (ctn.firstChild) ctn.removeChild(ctn.firstChild);
 }
